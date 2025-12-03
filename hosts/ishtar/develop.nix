0a90a4df3014a5853a36107d3e016a3f7b0bd01f
Sarah Lament <sarah@lament.gay>
2025-12-02 22:18:15 -0600

    treewide: nix formatter run caught something

--- hosts/ishtar/develop.nix
+++ hosts/ishtar/develop.nix
@@ -4,33 +4,33 @@
   pkgs,
   ...
 }: {
-    programs.virt-manager.enable = true;
-    virtualisation = {
-      docker.enable = true;
-      libvirtd = {
-        enable = true;
-        qemu.vhostUserPackages = with pkgs; [virtiofsd];
-      };
-      spiceUSBRedirection.enable = true;
+  programs.virt-manager.enable = true;
+  virtualisation = {
+    docker.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu.vhostUserPackages = with pkgs; [virtiofsd];
     };
+    spiceUSBRedirection.enable = true;
+  };
 
-    home-manager.sharedModules = [
-      {
-        dconf.settings = {
-          "org/virt-manager/virt-manager/connections" = {
-            autoconnect = ["qemu:///system"];
-            uris = ["qemu:///system"];
-          };
+  home-manager.sharedModules = [
+    {
+      dconf.settings = {
+        "org/virt-manager/virt-manager/connections" = {
+          autoconnect = ["qemu:///system"];
+          uris = ["qemu:///system"];
         };
-      }
-    ];
+      };
+    }
+  ];
 
-    environment.systemPackages = with pkgs; [
-      nixd # Nix language server
-      alejandra # Nix formatter
-      nodejs # JavaScript runtime
-      uv # Python package manager
-      python3 # Python interpreter
-      rustup # Rust toolchain installer
-    ];
+  environment.systemPackages = with pkgs; [
+    nixd # Nix language server
+    alejandra # Nix formatter
+    nodejs # JavaScript runtime
+    uv # Python package manager
+    python3 # Python interpreter
+    rustup # Rust toolchain installer
+  ];
 }
