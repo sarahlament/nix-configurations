a9024f82da25f9e1801af369adc01b0cf3037d56
Sarah Huggins <sarah@lament.gay>
2026-01-02 21:25:34 -0600

    finish dev

--- hosts-common/network.nix
+++ hosts-common/network.nix
@@ -21,20 +21,45 @@
 			PermitRootLogin = false;
     };
   };
-
-	systemd = {
-	  targets.tailnet-online = {
-		  description = "tailnet is connected";
-			after = ["network-online.target" "tailscaled.service"];
-			wants = ["network-online.target" "tailscaled.service"];
+  systemd = {
+    targets.tailnet-online = {
+      description = "tailnet is online and connected";
+      after = ["network-online.target" "tailscaled.service"];
+      wants = ["network-online.target" "tailscaled.service"];
     };
-	  services.sshd = {
-	    after = ["tailnet-online.target"];
-		  requires = ["tailnet-online.target"];
-		  serviceConfig = {
-        Restart = "always";
-			  RestartSec = "3s";
+    services = {
+      tailnet-ready = {
+        description = "check for tailnet connectivity";
+        after = ["tailscaled.service"];
+        wants = ["tailscaled.service"];
+        wantedBy = ["tailnet-online.target"];
+
+        serviceConfig = {
+          type = "oneshot";
+          RemainAfterExit = true;
+        };
+
+        script = ''
+          # wait for tailnet to be up
+          until ${pkgs.tailscale}/bin/tailscale status --json | \
+              ${pkgs.jq}/bin/jq -e '.BackendState == "Running"'; do
+            sleep 2
+          done
+
+          # wait for tailnet to have an IP
+          until ${pkgs.iproute2}/bin/ip addr show tailscale0 | grep -q '100\.'; do
+            sleep 2
+          done
+        '';
       };
-    };'
+	    sshd = {
+	      after = ["tailnet-online.target"];
+  		  requires = ["tailnet-online.target"];
+	  	  serviceConfig = {
+          Restart = "always";
+			    RestartSec = "3s";
+        };
+      };
+    };
   };
 }
