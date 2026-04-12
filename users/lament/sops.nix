f582bafc9f8be55c2b996df71eb6fc7157364104
Sarah Lament <sarah@lament.gay>
2026-04-11 20:52:17 -0500

    retated secrets

--- users/lament/sops.nix
+++ users/lament/sops.nix
@@ -7,10 +7,4 @@
   sops.age.keyFile = "/home/lament/.config/sops/age/keys.txt";
   sops.defaultSopsFile = ../../hosts/common/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
-
-  sops.secrets.context7ApiKey = {};
-  sops.templates.context7Env.content = ''
-    CONTEXT7_API_KEY=${config.sops.placeholder.context7ApiKey}
-  '';
-  programs.zsh.envExtra = "source ${config.sops.templates.context7Env.path}";
 }
