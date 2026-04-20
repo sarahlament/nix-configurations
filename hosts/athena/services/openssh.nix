769b454b2148566e02910889808ac39ad345dab9
Sarah Lament <sarah@lament.gay>
2026-04-19 23:35:08 -0500

    athena: emergency ssh on public interface added

--- hosts/athena/services/openssh.nix
+++ hosts/athena/services/openssh.nix
@@ -4,8 +4,9 @@
   pkgs,
   ...
 }: {
+  networking.firewall.allowedTCPPorts = [22];
   services.openssh.listenAddresses = [
-    # we only want to listen on the tailnet
+    {addr = "104.200.16.195";}
     {addr = "100.64.0.1";}
     {addr = "fd7a:115c:a1e0::1";}
   ];
