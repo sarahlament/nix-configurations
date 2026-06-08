7d189a145151e015a8ddbffa1ad2bebc2b66b190
Sarah Lament <sarah@lament.gay>
2026-06-07 19:45:04 -0500

    actually, let's use catppuccin everywhere

--- modules/nixosModules/profiles/stylix.nix
+++ modules/nixosModules/profiles/stylix.nix
@@ -20,6 +20,7 @@
         enable = true;
         enableReleaseChecks = false;
         image = mkIf cfg.wallpaper wallpaperSrc;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
         polarity = "dark";
 
         fonts = {
