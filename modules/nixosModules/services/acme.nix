15b3eb98013cc6b6fb8bb2aee2b1c5e5d7c1bbb7
Sarah Lament <sarah@lament.gay>
2026-06-21 23:36:12 -0500

    `acme` and `caddy` now use rfc2136 auth (#48)
    - `acmeTsig` template created to reference new DNS authentication metod
        - we use RFC2136 to authenticate to our hidden authority
    
    Reviewed-on: https://git.lament.gay/sarahlament/nix-configurations/pulls/48

--- modules/nixosModules/services/acme.nix
+++ modules/nixosModules/services/acme.nix
@@ -10,16 +10,13 @@
     ...
   }: let
     inherit (self.myLib.constants) fqdn;
-    inherit (self.myLib.helpers) mkSopsFile;
   in {
     sops = {
-      secrets = {
-        porkbunApi = {sopsFile = mkSopsFile "services";};
-        porkbunKey = {sopsFile = mkSopsFile "services";};
-      };
-      templates.porkbunAcme.content = ''
-        PORKBUN_API_KEY=${config.sops.placeholder.porkbunApi}
-        PORKBUN_SECRET_API_KEY=${config.sops.placeholder.porkbunKey}
+      templates.acmeTsig.content = ''
+        RFC2136_NAMESERVER=127.0.0.1:5353
+        RFC2136_TSIG_KEY=acme-rfc2136
+        RFC2136_TSIG_ALGORITHM=hmac-sha256.
+        RFC2136_TSIG_SECRET=${config.sops.placeholder.tsigSecret}
       '';
     };
 
@@ -27,8 +24,8 @@
       acceptTerms = true;
       defaults = {
         email = "sarah@${fqdn}";
-        dnsProvider = "porkbun";
-        environmentFile = config.sops.templates.porkbunAcme.path;
+        dnsProvider = "rfc2136";
+        environmentFile = config.sops.templates.acmeTsig.path;
       };
     };
   };
--- modules/nixosModules/services/caddy.nix
+++ modules/nixosModules/services/caddy.nix
@@ -16,8 +16,22 @@
       443 # HTTPS
     ];
 
+    systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.templates.acmeTsig.path;
     services.caddy = {
       enable = true;
+      package = pkgs.caddy.withPlugins {
+        plugins = ["github.com/caddy-dns/rfc2136@v1.0.0"];
+        hash = "sha256-i6hgT3ufiVz13f2Ruox7EPLhIDXSomA3T/4IFmoHJUo=";
+      };
+
+      globalConfig = ''
+        acme_dns rfc2136 {
+          key_name {env.RFC2136_TSIG_KEY}
+          key_alg {env.RFC2136_TSIG_ALGORITHM}
+          key {env.RFC2136_TSIG_SECRET}
+          server {env.RFC2136_NAMESERVER}
+        }
+      '';
       virtualHosts.${fqdn} = {
         extraConfig = ''
           root * /var/www/${fqdn}
